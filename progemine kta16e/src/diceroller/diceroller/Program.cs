5d2811a66b6eda6529ecf22ca8ef8dc6b57fa8d6
rasmusprees <rasmus.prees@gmail.com>
2017-11-12 17:24:16 +0200

    assignment#9

--- progemine kta16e/src/diceroller/diceroller/Program.cs
+++ progemine kta16e/src/diceroller/diceroller/Program.cs
@@ -9,22 +9,23 @@ namespace diceroller
             Console.WriteLine("> /roll 3d6 2d8");
             Console.WriteLine();
 
-            dice dice = new dice();
+            Dice d6 = new Dice(6);
 
             var total = 0;
 
             for (var i = 0; i < 3; i++)
             {
                 
-                var roll = dice.Roll(6);
+                var roll = d6.Roll();
                 total += roll;
 
                 Console.WriteLine($"1d6: { roll }");
             }
 
+            Dice d8 = new Dice(8);
             for (var i = 0; i < 2; i++)
             {
-                var roll = dice.Roll(8);
+                var roll = d8.Roll();
                 total += roll;
 
                 Console.WriteLine($"1d8: { roll }");
--- progemine kta16e/src/diceroller/diceroller/dice.cs
+++ progemine kta16e/src/diceroller/diceroller/dice.cs
@@ -6,15 +6,17 @@ namespace diceroller
 {
     using System;
 
-    public class dice
+    public class Dice
     {
         private Random Random { get; } = new Random();
 
-        public int Roll(int sides)
+        public Dice(int sides)
         {
-            var random = new Random();
 
-            return this.Random.Next(1, sides + 1); // max value is exclusive, so we have to add +1
+        }
+            public int Roll()
+            {
+                return 0;
+            }
         }
     }
-}
