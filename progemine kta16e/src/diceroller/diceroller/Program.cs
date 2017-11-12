809e58b85a782f0eaf0ff47ccc50c78e693143b9
rasmusprees <rasmus.prees@gmail.com>
2017-11-12 16:12:35 +0200

    assignment#1

---
+++ progemine kta16e/src/diceroller/diceroller/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace diceroller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("> /roll 3d6 2d8");
+            Console.WriteLine();
+
+            Console.WriteLine("1d6: 5");
+            Console.WriteLine("1d6: 3");
+            Console.WriteLine("1d6: 2");
+            Console.WriteLine("1d8: 6");
+            Console.WriteLine("1d8: 4");
+            Console.WriteLine();
+
+            Console.WriteLine("Roll total: 20");
+            Console.WriteLine();
+            Console.WriteLine(">");
+            Console.ReadLine();
+        }
+    }
+}
