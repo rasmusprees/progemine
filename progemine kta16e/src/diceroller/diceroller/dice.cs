55704e04501158af535ead3d114d592cdfba0bb2
rasmusprees <rasmus.prees@gmail.com>
2017-11-12 19:34:47 +0200

    assignment#17

--- progemine kta16e/src/diceroller/diceroller/dice.cs
+++ progemine kta16e/src/diceroller/diceroller/dice.cs
@@ -13,7 +13,7 @@ namespace DiceRoller
 
         public string Description { get; }
 
-        private Random Random { get; } = new Random();
+        private static Random Random { get; } = new Random();
 
         private Dice(int sides, string description)
         {
@@ -23,13 +23,13 @@ namespace DiceRoller
 
         public static Dice D6 => new Dice(6, "d6");
 
-        public static Dice D8 => new Dice(8, "d8");
+        public static Dice D8 => new Dice(6, "d8");
 
         public int Roll()
             {
-                return this.Random.Next(1, this.Sides + 1);
+                return Dice.Random.Next(1, this.Sides + 1); // max value is exlusive, so we have to add +1
 
-            }
+        }
             
 
     }
